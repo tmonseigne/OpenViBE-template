da4d80a9c73386eeadc2ecad55de600f5c521b62
Thibaut <thibaut.monseigne@inria.fr>
2020-07-16 17:04:15 +0200

    :sparkles: Change finished to pass OV 2.2 to OV 3.0

--- src/boxes/CHelloWorld.cpp
+++ src/boxes/CHelloWorld.cpp
@@ -4,20 +4,24 @@ namespace OpenViBE {
 namespace Plugins {
 namespace Template {
 
+///-------------------------------------------------------------------------------------------------
 uint64_t CHelloWorld::getClockFrequency()
 {
 	const double seconds = FSettingValueAutoCast(*this->getBoxAlgorithmContext(), 0);
 	// We need the freq in 32:32 fixed point time
 	return CTime::fromSeconds(seconds);
 }
+///-------------------------------------------------------------------------------------------------
 
+///-------------------------------------------------------------------------------------------------
 bool CHelloWorld::processClock(CMessage& /*msg*/)
 {
 	const CString myGreeting = FSettingValueAutoCast(*this->getBoxAlgorithmContext(), 1);
 	getLogManager() << Kernel::LogLevel_Info << ": " << myGreeting << "\n";
 	return true;
 }
+///-------------------------------------------------------------------------------------------------
 
-}
-}
-}
+}  // namespace Template
+}  // namespace Plugins
+}  // namespace OpenViBE
