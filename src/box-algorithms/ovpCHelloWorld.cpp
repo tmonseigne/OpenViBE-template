0d8e3584d0d4e50191c6a37fe117e54067a512c4
Thibaut <thibaut.monseigne@inria.fr>
2020-07-16 16:34:43 +0200

    :bug: Use CTime Function
    Because Time uniformisation

--- src/box-algorithms/ovpCHelloWorld.cpp
+++ src/box-algorithms/ovpCHelloWorld.cpp
@@ -4,7 +4,6 @@
 #include "ovpCHelloWorld.h"
 
 #include <cstdlib>		// atof
-#include <openvibe/ovITimeArithmetics.h>
 
 namespace OpenViBE {
 namespace Plugins {
@@ -15,7 +14,7 @@ uint64_t CHelloWorld::getClockFrequency()
 	const double l_f64Frequency = FSettingValueAutoCast(*this->getBoxAlgorithmContext(), 0);
 
 	// We need the freq in 32:32 fixed point time
-	return ITimeArithmetics::secondsToTime(l_f64Frequency);
+	return CTime::fromSeconds(l_f64Frequency);
 }
 
 void CHelloWorld::release() { delete this; }
